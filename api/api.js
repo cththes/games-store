2573382648972d25ecdf629135069ca1b82faff5
cth <cththes@gmail.com>
2022-08-06 01:56:28 +0300

    add authAPI #10

--- api/api.js
+++ api/api.js
@@ -4,3 +4,12 @@ const instance = axios.create({
    withCredentials: true,
    baseURL: `http://localhost:1337`,
 });
+
+export const authAPI = {
+   signup(username, email, password) {
+      return instance.post(`api/auth/local/register`, { username, email, password })
+   },
+   login(username, password) {
+      return instance.post(`/api/auth/local`, { username, password });
+   },
+};
