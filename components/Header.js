ce06a3cdfe8831a8a6815abd399d5891c8bef8e4
cth <cththes@gmail.com>
2022-08-08 15:03:02 +0300

    redirect fix #10

--- components/Header.js
+++ components/Header.js
@@ -4,15 +4,16 @@ import styles from "../styles/Header.module.scss";
 import auth from "../store/auth";
 import { LoginButton } from '../pages/login';
 import { SignupButton } from '../pages/signup';
-import Router from "next/router";
+import { useRouter } from 'next/router'
 
 
 const Header = () => {
+   const Router = useRouter()
    return (
       <div className={styles.header}>
          {auth.isAuth && <Logout />}
          {(!auth.isAuth && Router.pathname === "/login") && <SignupButton />}
-         {(!auth.isAuth && !Router.pathname === "/login") && <LoginButton />}
+         {(!auth.isAuth && Router.pathname !== "/login") && <LoginButton />}
       </div>
    )
 }
