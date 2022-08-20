5402102437e39a4a6b97d6b6808dcce70d81373a
cth <cththes@gmail.com>
2022-08-20 17:45:24 +0300

    #11 add login graphql

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
 import '../styles/globals.css'
 import 'antd/dist/antd.css'
 import Header from '../components/Header'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+   uri: 'http://localhost:1337/graphql',
+   cache: new InMemoryCache(),
+ });
 
 function MyApp({ Component, pageProps }) {
    return (
-      <div>
+      <ApolloProvider client={client}>
          <Header />
          <Component {...pageProps} />
-      </div>
+      </ApolloProvider>
    )
 }
 
--- pages/login.js
+++ pages/login.js
@@ -3,20 +3,40 @@ import { authAPI } from '../api/api';
 import auth from "../store/auth";
 import Router from "next/router";
 import { Button } from 'antd';
+import { useMutation, gql } from '@apollo/client';
+import {saveJwt} from "../api/utils";
+
+
+const LOGIN_MUTATION = gql`
+   mutation login($input: UsersPermissionsLoginInput!){
+      login(input: $input) {
+      jwt
+      user {
+         id
+         username
+         email
+         confirmed
+         blocked
+      }
+      }
+   }
+`;
 
 export const LoginButton = () => {
-   const onLoginButtonClick = () =>{
+   const onLoginButtonClick = () => {
       Router.push("/login")
    }
-   return(
+   return (
       <Button type="primary" onClick={onLoginButtonClick}>Войти</Button>
    )
 }
 
 const Login = () => {
+   const [loginGql, { data, loading, error }] = useMutation(LOGIN_MUTATION);
+
    const onSubmit = (values) => {
-      const {email, password} = values
-      authAPI.login(email, password)
+      const { email, password } = values
+      /*authAPI.login(email, password)
          .then((response) => {
             if (response.status === 200) {
                auth.login()
@@ -25,9 +45,28 @@ const Login = () => {
             if (auth.isAuth && pathname === "/login") {
                Router.push("/");
             }
-         });
+         });*/
+      loginGql({
+         variables: {
+            input: {
+               identifier: email,
+               password: password,
+               provider: "local"
+            }
+         }
+      }).then((response) => {
+         console.log("response",response)
+         if (response.data.login.jwt) {
+            saveJwt(response.data.login.jwt)
+            auth.login()
+         }
+         const { pathname } = Router;
+         console.log(".then auth.isAuth", auth.isAuth)
+         if (auth.isAuth && pathname === "/login") {
+            Router.push("/");
+         }
+      })
    }
-
    return (
       <div className='login'>
          <div className="login__header">Авторизация</div>
